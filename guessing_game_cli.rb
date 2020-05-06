2487c01a3d4d354164099a8ec1b7a66b955f7683
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-06 17:26:55 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,11 +6,12 @@ def run_guessing_game
   
   random = rand(6) + 1 
   
-  input = gets
+  input = gets.chomp
   
   if input == "exit"
     puts "Goodbye!"
     elsif input != random
+ 
    puts  "Sorry! The computer guessed #{random}.chomp" 
  end
    
